dc7bb19aa466fd91dad42404faec26dc382de423
SSead <smailagisead@gmail.com>
2022-06-03 13:14:42 +0200

    Initial commit

---
+++ app/src/main/java/ba/ibu/gram/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ba.ibu.gram.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
