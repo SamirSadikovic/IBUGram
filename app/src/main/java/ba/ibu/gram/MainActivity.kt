def30af6f58b7be28189fcd9b7a4b8fc93ddd66a
SSead <smailagisead@gmail.com>
2022-06-06 09:01:14 +0200

    feat(setup): Theme setup

--- app/src/main/java/ba/ibu/gram/MainActivity.kt
+++ app/src/main/java/ba/ibu/gram/MainActivity.kt
@@ -1,25 +1,29 @@
 package ba.ibu.gram
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import ba.ibu.gram.ui.theme.IBUgramTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import ba.ibu.gram.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
+
     setContent {
-      IBUgramTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
+      AppTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          Burch()
         }
       }
     }
@@ -27,14 +31,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Burch() {
+  Image(painter = painterResource(R.drawable.ic_burch), contentDescription = null)
 }
 
-@Preview(showBackground = true)
+@Preview(
+  name = "Burch",
+  showBackground = true
+)
 @Composable
 fun DefaultPreview() {
-  IBUgramTheme {
-    Greeting("Android")
+  AppTheme {
+    Burch()
+  }
+}
+
+@Preview(
+  name = "Burch Night",
+  showBackground = true, uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun DarkDefaultPreview() {
+  AppTheme {
+    Burch()
   }
 }
--- app/src/main/java/ba/ibu/gram/ui/theme/Shape.kt
+++ app/src/main/java/ba/ibu/gram/ui/theme/Shape.kt
@@ -1,7 +1,7 @@
 package ba.ibu.gram.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
